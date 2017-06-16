3a307a02e0bc3301eb8d4033b270c0542d23a861
pritesh-voalnsys <pritesh.prajapati@volansystech.com>
2017-06-16 18:44:21 +0530

    New enhance for Shopping

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , EventEmitter , Output} from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,14 +8,18 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+@Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('A Sample Recipe', 'city corner', 'src/Image/vol.png'),
-    new Recipe('A Sample Recipe', 'city corner', 'src/Image/vol.png')
+    new Recipe('A Sample Recipe 2', 'city corner', 'src/Image/vol.png')
   ];
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  onRecipeSelected(recipe : Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
